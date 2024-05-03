0f2be2fc607032c2ee9e75b36b5dd509ed09319a
DaMon14 <david.monoran@gmail.com>
2024-05-03 17:43:30 +0300

    gitignore added

--- codptmicromousec++/Source.cpp
+++ codptmicromousec++/Source.cpp
@@ -9,24 +9,6 @@ struct cell_walls {
     bool right;
     bool down;
 };
-/*
-int drum_maze[16][16];
-void drum(int x, int y) {
-    drum_maze[x][y] = dist_maze[x][y];
-    if (maze[x][y].up == false && dist_maze[x - 1][y] == drum_maze[x][y] - 1) {
-        drum(x - 1, y);
-    }
-    if (maze[x][y].left == false && dist_maze[x][y - 1] == drum_maze[x][y] - 1) {
-        drum(x, y - 1);
-    }
-    if (maze[x][y].right == false && dist_maze[x][y + 1] == drum_maze[x][y] - 1) {
-        drum(x, y + 1);
-    }
-    if (maze[x][y].down == false && dist_maze[x + 1][y] == drum_maze[x][y] - 1) {
-        drum(x + 1, y);
-    }
-}
-*/
 
 cell_walls maze[16][16] = {
 {1,1,1,0, 1,1,1,0, 1,1,1,0, 1,1,0,0, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,0,1, 0,1,1,0},
@@ -81,28 +63,28 @@ vector<pair<int, int>> findBestRoute(int start_x, int start_y) {
     vector<pair<int, int>> best_route;
     int x = start_x, y = start_y;
     while (dist_maze[x][y] > 0) {
-        cout << "Exploring cell: (" << x << ", " << y << ")" << endl;
+        //cout << "Exploring cell: (" << x << ", " << y << ")" << endl;
         best_route.push_back({ x, y });
         // Choose the neighboring cell with the next lowest cost
         if (x > 0 && dist_maze[x - 1][y] == dist_maze[x][y] - 1) {
-            cout << "Selected neighbor: (" << x - 1 << ", " << y << ")" << endl;
+            //cout << "Selected neighbor: (" << x - 1 << ", " << y << ")" << endl;
             x--;
         }
         else if (y > 0 && dist_maze[x][y - 1] == dist_maze[x][y] - 1) {
-            cout << "Selected neighbor: (" << x << ", " << y - 1 << ")" << endl;
+            //cout << "Selected neighbor: (" << x << ", " << y - 1 << ")" << endl;
             y--;
         }
         else if (x < 15 && dist_maze[x + 1][y] == dist_maze[x][y] - 1) {
-            cout << "Selected neighbor: (" << x + 1 << ", " << y << ")" << endl;
+            //cout << "Selected neighbor: (" << x + 1 << ", " << y << ")" << endl;
             x++;
         }
         else if (y < 15 && dist_maze[x][y + 1] == dist_maze[x][y] - 1) {
-            cout << "Selected neighbor: (" << x << ", " << y + 1 << ")" << endl;
+            //cout << "Selected neighbor: (" << x << ", " << y + 1 << ")" << endl;
             y++;
         }
         else {
             // No valid neighboring cell found
-            cout << "Error: No valid neighboring cell found from (" << x << ", " << y << ")" << endl;
+            //cout << "Error: No valid neighboring cell found from (" << x << ", " << y << ")" << endl;
             return {}; // Return an empty vector indicating failure
         }
     }
@@ -115,48 +97,6 @@ vector<pair<int, int>> findBestRoute(int start_x, int start_y) {
 
     return best_route;
 }
-
-
-vector<string> generateMovementInstructions(const vector<pair<int, int>>& best_route) {
-    vector<string> instructions;
-
-    // Start generating movement instructions from the second last point to the first point
-    for (size_t i = best_route.size() - 1; i > 0; --i) {
-        int curr_x = best_route[i].first;
-        int curr_y = best_route[i].second;
-        int next_x = best_route[i - 1].first;
-        int next_y = best_route[i - 1].second;
-
-        // Calculate the change in x and y coordinates
-        int dx = next_x - curr_x;
-        int dy = next_y - curr_y;
-
-        // Generate movement instruction based on the change in coordinates
-        if (dx == 1) {
-            instructions.push_back("Move forward");
-        }
-        else if (dx == -1) {
-            instructions.push_back("Move backward");
-        }
-        else if (dy == 1) {
-            instructions.push_back("Move right");
-        }
-        else if (dy == -1) {
-            instructions.push_back("Move left");
-        }
-        else {
-            instructions.push_back("Invalid movement");
-        }
-    }
-
-    return instructions;
-}
-
-
-
-
-
-
 
 int main()
 {
@@ -198,14 +138,39 @@ int main()
         cout << "(" << best_route[i].first << ", " << best_route[i].second << ") ";
     }
     cout << endl;
-    vector<string> instructions = generateMovementInstructions(best_route);
 
-    // Display the movement instructions
-    cout << "Movement Instructions:" << endl;
-    for (const string& instruction : instructions) {
-        cout << instruction << endl;
+    vector<int> directions;
+    for (int i = best_route.size()-1; i >0; i--) {
+        if (best_route[i - 1].first < best_route[i].first) {
+            directions.push_back(1);
+        }
+        else if (best_route[i - 1].first > best_route[i].first) {
+            directions.push_back(3);
+        }
+        else if (best_route[i - 1].second > best_route[i].second) {
+            directions.push_back(2);
+        }
+        else if (best_route[i - 1].second < best_route[i].second) {
+            directions.push_back(0);
+        }
     }
-
+    int cnt = 1;
+    for (int i = 1; i < directions.size(); i++) {
+        if (directions[i - 1] == directions[i]) {
+            cnt++;
+        }
+        else {
+            cout << "m" <<cnt*160<< endl;
+            cnt = 1;
+            if (directions[i - 1] < (directions[i] - 1) % 4) {
+                cout << "r90"<<endl;
+            }
+            else if (directions[i - 1] > (directions[i] - 1) % 4) {
+                cout << "r-90"<<endl;
+            }
+        }
+    }
+    cout << "m160";
 
 }
 
