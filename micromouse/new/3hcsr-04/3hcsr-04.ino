4bff7c76302386e833758481fe3c84e829e560b3
DaMon14 <david.monoran@gmail.com>
2024-05-04 12:27:00 +0300

    get walls

--- micromouse/new/3hcsr-04/3hcsr-04.ino
+++ micromouse/new/3hcsr-04/3hcsr-04.ino
@@ -8,6 +8,14 @@ const int echoPinD = 33;
 
 
 float distanceS, distanceM, distanceD;
+bool wallS, wallM, wallD;
+int distforwall = 6;
+
+struct cell{
+  bool wallS;
+  bool wallM;
+  bool wallD;
+};
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -21,13 +29,51 @@ void loop() {
   distanceS = get_sensor(echoPinS);
   distanceM = get_sensor(echoPinM);
   distanceD = get_sensor(echoPinD);
-  Serial.print(distanceS);
+  if(distanceS < 6){//in cell dist from sensor to wall is 3.1cm
+    wallS = 1;
+  }else{
+    wallS = 0;
+  }
+  if(distanceM < 6){
+    wallM = 1;
+  }else{
+    wallM = 0;
+  }
+  if(distanceD < 6){
+    wallD = 1;
+  }else{
+    wallD = 0;
+  }
+  Serial.print(wallS);
   Serial.print(",");
-  Serial.print(distanceM);
+  Serial.print(wallM);
   Serial.print(",");
-  Serial.print(distanceD);
+  Serial.print(wallD);
   Serial.println("");
   //delay(100);
+}
+
+cell get_walls(){
+  cell aux;
+  distanceS = get_sensor(echoPinS);
+  distanceM = get_sensor(echoPinM);
+  distanceD = get_sensor(echoPinD);
+  if(distanceS < 6){//in cell dist from sensor to wall is 3.1cm
+    aux.wallS = 1;
+  }else{
+    aux.wallS = 0;
+  }
+  if(distanceM < 6){
+    aux.wallM = 1;
+  }else{
+    aux.wallM = 0;
+  }
+  if(distanceD < 6){
+    aux.wallD = 1;
+  }else{
+    aux.wallD = 0;
+  }
+  return aux;
 }
 
 int get_sensor(int pin){
