413b1297f2d33c4f1fe98ad5ecfe3897f57be2c1
DaMon14 <david.monoran@gmail.com>
2024-05-07 23:04:05 +0300

    small changes

--- sacr/servo_regalre/servo_regalre.ino
+++ sacr/servo_regalre/servo_regalre.ino
@@ -7,8 +7,15 @@ void setup() {
 }
 
 void loop() {
+  //110 - perpendicular base
+  // 0 tray mid servo
+  //90 upright
+  //180 - grip
+  //70 grip base
   myservo.write(110);                  // sets the servo position according to the scaled value
   delay(1000); 
-  myservo.write(90);                  // sets the servo position according to the scaled value
+  myservo.write(75);                  // sets the servo position according to the scaled value
   delay(1000);// waits for the servo to get there
+  //myservo.write(0);                  // sets the servo position according to the scaled value
+  //delay(1000);// waits for the servo to get there
 }
