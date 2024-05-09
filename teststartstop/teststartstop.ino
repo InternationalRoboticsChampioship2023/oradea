626172b5b840fc294a46aab12d73643456315ee5
Daniel <pdapdvpd@gmail.com>
2024-05-09 23:14:50 +0300

    no ni ma la el ma

--- teststartstop/teststartstop.ino
+++ teststartstop/teststartstop.ino
@@ -1,22 +1,32 @@
+#include <IRremote.h>
 
+const int RECV_PIN = 6;
 
-//#define startstopbutton A1;
+IRrecv irrecv(RECV_PIN);
 
+decode_results results;
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(2,INPUT);
-
-  Serial.begin(9600);   
-  
-
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  irrecv.blink13(true);
 }
 
 void loop() {
-  
-if(digitalRead(2)==HIGH){
-  Serial.print("ioi");
-}
-  
-
+  if (irrecv.decode(&results)) {
+    if (results.decode_type == NEC) {
+      Serial.print("NEC: ");
+    } else if (results.decode_type == SONY) {
+      Serial.print("SONY: ");
+    } else if (results.decode_type == RC5) {
+      Serial.print("RC5: ");
+    } else if (results.decode_type == RC6) {
+      Serial.print("RC6: ");
+    } else if (results.decode_type == UNKNOWN) {
+      Serial.print("UNKNOWN: ");
+    }
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+  }
 }
